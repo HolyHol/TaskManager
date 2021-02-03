918619c5679061baa3be20a22bfde9e8fea4467a
Rena <nenavigygygl@gmail.com>
2021-02-03 17:42:41 +0100

    Added state machine

--- app/models/task.rb
+++ app/models/task.rb
@@ -6,4 +6,25 @@ class Task < ApplicationRecord
   validates :description, presence: true
   validates :author, presence: true
   validates :description, length: { maximum: 500 }
+
+  state_machine initial: :new_task do
+    event :start_doing do
+      transition [:new_task, :in_qa, :in_code_review] => :in_development
+    end
+    event :archive do
+      transition [:new_task, :released] => :archived
+    end
+    event :to_in_qa do
+      transition [:in_development] => :in_qa
+    end
+    event :to_code_review do
+      transition [:in_qa] => :in_code_review
+    end
+    event :to_prerelease do
+      transition [:in_code_review] => :ready_for_release
+    end
+    event :release do
+      transition [:ready_for_release] => :released
+    end
+  end
 end
