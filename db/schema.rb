bddf0fcecd145fa2f71ff64986478b230cb43aec
Rena <nenavigygygl@gmail.com>
2021-02-12 14:52:33 +0100

    add migration with converting this fields into Foreign Keys

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_28_123446) do
+ActiveRecord::Schema.define(version: 2021_02_12_121440) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,4 +37,6 @@ ActiveRecord::Schema.define(version: 2021_01_28_123446) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "tasks", "users", column: "assignee_id"
+  add_foreign_key "tasks", "users", column: "author_id"
 end
