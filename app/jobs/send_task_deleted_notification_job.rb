2591a4de0d5e754493098315aa76e2d7b600ac72
Rena <nenavigygygl@gmail.com>
2021-06-28 21:55:40 +0200

    Add sidekiq unique

--- app/jobs/send_task_deleted_notification_job.rb
+++ app/jobs/send_task_deleted_notification_job.rb
@@ -1,6 +1,7 @@
 class SendTaskDeletedNotificationJob < ApplicationJob
   sidekiq_options queue: :mailers
   sidekiq_throttle_as :mailer
+  sidekiq_options lock: :until_and_while_executing, on_conflict: { client: :log, server: :reject }
 
   def perform(task_id, user_id)
     user = User.find(user_id)
--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,6 +1,7 @@
 require 'sidekiq/web'
 require "sidekiq/throttled"
 require "sidekiq/throttled/web"
+require 'sidekiq_unique_jobs/web'
 
 Sidekiq.configure_server do |config|
   config.redis = { url: ENV['REDIS_URL'] }
@@ -13,3 +14,4 @@ end
 Sidekiq::Throttled.setup!
 
 Sidekiq::Throttled::Registry.add(:mailer, { threshold: { limit: 1, period: 5.seconds } })
+
