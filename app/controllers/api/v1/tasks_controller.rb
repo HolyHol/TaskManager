08e402b5986d124f929d89388a3d54212cc817de
Rena <nenavigygygl@gmail.com>
2021-07-01 12:54:28 +0200

    Add test and image attachments

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -1,10 +1,10 @@
 class Api::V1::TasksController < Api::V1::ApplicationController
   def index
-    tasks = Task.all
-                .ransack(ransack_params)
-                .result
-                .page(page)
-                .per(per_page)
+    tasks = Task.all.
+      ransack(ransack_params).
+      result.
+      page(page).
+      per(per_page)
 
     respond_with(tasks, each_serializer: TaskSerializer, root: 'items', meta: build_meta(tasks))
   end
@@ -21,7 +21,7 @@ class Api::V1::TasksController < Api::V1::ApplicationController
     task.save
 
     if task.save
-     SendTaskCreateNotificationJob.perform_async(task.id)
+      SendTaskCreateNotificationJob.perform_async(task.id)
     end
 
     respond_with(task, serializer: TaskSerializer, location: nil)
@@ -39,17 +39,43 @@ class Api::V1::TasksController < Api::V1::ApplicationController
 
   def destroy
     task = Task.find(params[:id])
-    user = task.author 
+    user = task.author
     if task.destroy
-     SendTaskDeletedNotificationJob.perform_async(task.id, user.id)
+      SendTaskDeletedNotificationJob.perform_async(task.id, user.id)
     end
 
     respond_with(task)
+  end
+
+  def attach_image
+    task = Task.find(params[:id])
+    task_attach_image_form = TaskAttachImageForm.new(attachment_params)
+
+    if task_attach_image_form.invalid?
+      respond_with(task_attach_image_form)
+      return
+    end
+
+    image = task_attach_image_form.processed_image
+    task.image.attach(image)
+
+    respond_with(task, serializer: TaskSerializer)
+  end
+
+  def remove_image
+    task = Task.find(params[:id])
+    task.image.purge
+
+    respond_with(task, serializer: TaskSerializer)
   end
 
   private
 
   def task_params
     params.require(:task).permit(:name, :description, :assignee_id, :state_event)
+  end
+
+  def attachment_params
+    params.require(:attachment).permit(:image, :crop_x, :crop_y, :crop_width, :crop_height)
   end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,7 +1,8 @@
 class Task < ActiveRecord::Base
-
   belongs_to :author, class_name: 'User'
   belongs_to :assignee, class_name: 'User', optional: true
+
+  has_one_attached :image
 
   validates :name, presence: true
   validates :description, presence: true
